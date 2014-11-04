0207bd984916d218c964254870ec78aa8196c43c
hibikiledo <hibikiledo@hibikiledos-MacBook-Pro.local>
2014-11-04 17:55:00 +0700

    Add instructions into table.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var arithmeticOps = JSON.parse('[{"instruction":"ADD","oprands":"A,Rn","description":"Add register to Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":0},{"instruction":"ADD","oprands":"A,direct","description":"Add direct byte to Accumulator","oscillatorPeriod":12,"sizeInByte":2,"ID":1},{"instruction":"ADD","oprands":"A,@Ri","description":"Add indirect RAM to Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":2},{"instruction":"ADD","oprands":"A,#data","description":"Add immediate data to Accumulator","oscillatorPeriod":12,"sizeInByte":2,"ID":3},{"instruction":"A DDC","oprands":"A,Rn","description":"Add register to Accumulator with Carry","oscillatorPeriod":12,"sizeInByte":1,"ID":4},{"instruction":"A DDC","oprands":"A,direct","description":"Add direct byte to Accumulator with Carry","oscillatorPeriod":12,"sizeInByte":2,"ID":5},{"instruction":"A DDC","oprands":"A,@Ri","description":"Add indirect RAM to Accumulator with Carry","oscillatorPeriod":12,"sizeInByte":1,"ID":6},{"instruction":"A DDC","oprands":"A,#data","description":"Add immediate data to Acc with Carry","oscillatorPeriod":12,"sizeInByte":2,"ID":7},{"instruction":"SUBB","oprands":"A,Rn","description":"Subtract Register from Acc with borrow","oscillatorPeriod":12,"sizeInByte":1,"ID":8},{"instruction":"SUBB","oprands":"A,direct","description":"Subtract direct byte from Acc with borrow","oscillatorPeriod":12,"sizeInByte":2,"ID":9},{"instruction":"SUBB","oprands":"A,@Ri","description":"Subtract indirect RAM from ACC with borrow","oscillatorPeriod":12,"sizeInByte":1,"ID":10},{"instruction":"SUBB","oprands":"A,#data","description":"Subtract immediate data from Acc with borrow","oscillatorPeriod":12,"sizeInByte":2,"ID":11},{"instruction":"INC","oprands":"A","description":"Increment Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":12},{"instruction":"INC","oprands":"Rn","description":"Increment register","oscillatorPeriod":12,"sizeInByte":1,"ID":13},{"instruction":"INC","oprands":"direct","description":"Increment direct byte","oscillatorPeriod":12,"sizeInByte":2,"ID":14},{"instruction":"INC","oprands":"@Ri","description":"Increment direct RAM","oscillatorPeriod":12,"sizeInByte":1,"ID":15},{"instruction":"DEC","oprands":"A","description":"Decrement Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":16},{"instruction":"DEC","oprands":"Rn","description":"Decrement Register","oscillatorPeriod":12,"sizeInByte":1,"ID":17},{"instruction":"DEC","oprands":"direct","description":"Decrement direct byte","oscillatorPeriod":12,"sizeInByte":2,"ID":18},{"instruction":"DEC","oprands":"@Ri","description":"Decrement indirect RAM","oscillatorPeriod":12,"sizeInByte":1,"ID":19},{"instruction":"INC","oprands":"DPTR","description":"Increment Data Pointer","oscillatorPeriod":24,"sizeInByte":1,"ID":20},{"instruction":"MUL","oprands":"AB","description":"Multiply A \u0026 B","oscillatorPeriod":48,"sizeInByte":1,"ID":21},{"instruction":"DIV","oprands":"AB","description":"Divide A by B","oscillatorPeriod":48,"sizeInByte":1,"ID":22},{"instruction":"DA","oprands":"A","description":"Decimal Adjust Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":23}]');
+var arithmeticOps = JSON.parse('[{"instruction":"ADD","oprands":"A,Rn","description":"Add register to Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":0},{"instruction":"ADD","oprands":"A,direct","description":"Add direct byte to Accumulator","oscillatorPeriod":12,"sizeInByte":2,"ID":1},{"instruction":"ADD","oprands":"A,@Ri","description":"Add indirect RAM to Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":2},{"instruction":"ADD","oprands":"A,#data","description":"Add immediate data to Accumulator","oscillatorPeriod":12,"sizeInByte":2,"ID":3},{"instruction":"ADDC","oprands":"A,Rn","description":"Add register to Accumulator with Carry","oscillatorPeriod":12,"sizeInByte":1,"ID":4},{"instruction":"ADDC","oprands":"A,direct","description":"Add direct byte to Accumulator with Carry","oscillatorPeriod":12,"sizeInByte":2,"ID":5},{"instruction":"ADDC","oprands":"A,@Ri","description":"Add indirect RAM to Accumulator with Carry","oscillatorPeriod":12,"sizeInByte":1,"ID":6},{"instruction":"ADDC","oprands":"A,#data","description":"Add immediate data to Acc with Carry","oscillatorPeriod":12,"sizeInByte":2,"ID":7},{"instruction":"SUBB","oprands":"A,Rn","description":"Subtract Register from Acc with borrow","oscillatorPeriod":12,"sizeInByte":1,"ID":8},{"instruction":"SUBB","oprands":"A,direct","description":"Subtract direct byte from Acc with borrow","oscillatorPeriod":12,"sizeInByte":2,"ID":9},{"instruction":"SUBB","oprands":"A,@Ri","description":"Subtract indirect RAM from ACC with borrow","oscillatorPeriod":12,"sizeInByte":1,"ID":10},{"instruction":"SUBB","oprands":"A,#data","description":"Subtract immediate data from Acc with borrow","oscillatorPeriod":12,"sizeInByte":2,"ID":11},{"instruction":"INC","oprands":"A","description":"Increment Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":12},{"instruction":"INC","oprands":"Rn","description":"Increment register","oscillatorPeriod":12,"sizeInByte":1,"ID":13},{"instruction":"INC","oprands":"direct","description":"Increment direct byte","oscillatorPeriod":12,"sizeInByte":2,"ID":14},{"instruction":"INC","oprands":"@Ri","description":"Increment direct RAM","oscillatorPeriod":12,"sizeInByte":1,"ID":15},{"instruction":"DEC","oprands":"A","description":"Decrement Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":16},{"instruction":"DEC","oprands":"Rn","description":"Decrement Register","oscillatorPeriod":12,"sizeInByte":1,"ID":17},{"instruction":"DEC","oprands":"direct","description":"Decrement direct byte","oscillatorPeriod":12,"sizeInByte":2,"ID":18},{"instruction":"DEC","oprands":"@Ri","description":"Decrement indirect RAM","oscillatorPeriod":12,"sizeInByte":1,"ID":19},{"instruction":"INC","oprands":"DPTR","description":"Increment Data Pointer","oscillatorPeriod":24,"sizeInByte":1,"ID":20},{"instruction":"MUL","oprands":"AB","description":"Multiply A \u0026 B","oscillatorPeriod":48,"sizeInByte":1,"ID":21},{"instruction":"DIV","oprands":"AB","description":"Divide A by B","oscillatorPeriod":48,"sizeInByte":1,"ID":22},{"instruction":"DA","oprands":"A","description":"Decimal Adjust Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":23}]');
 
 var datatransferOps = JSON.parse('[{"instruction":"MOV","oprands":"A,Rn","description":"Move register to Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":0},{"instruction":"MOV","oprands":"A,direct","description":"Move direct byte to Accumulator","oscillatorPeriod":12,"sizeInByte":2,"ID":1},{"instruction":"MOV","oprands":"A,@Ri","description":"Move indirect RAM to Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":2},{"instruction":"MOV","oprands":"A,#data","description":"Move immediate data to Accumulator","oscillatorPeriod":12,"sizeInByte":2,"ID":3},{"instruction":"MOV","oprands":"Rn,#data","description":"Move immediate data to register","oscillatorPeriod":12,"sizeInByte":2,"ID":4},{"instruction":"MOV","oprands":"direct,A","description":"Move Accumulator to direct byte","oscillatorPeriod":12,"sizeInByte":2,"ID":5},{"instruction":"MOV","oprands":"direct,Rn","description":"Move register to direct byte","oscillatorPeriod":24,"sizeInByte":2,"ID":6},{"instruction":"MOV","oprands":"direct, direct","description":"Move direct byte to direct","oscillatorPeriod":24,"sizeInByte":3,"ID":7},{"instruction":"MOV","oprands":"direct,@Ri","description":"Move indirectRAM to direct byte","oscillatorPeriod":24,"sizeInByte":2,"ID":8},{"instruction":"MOV","oprands":"direct,#data","description":"Move immediate data to direct byte","oscillatorPeriod":24,"sizeInByte":3,"ID":9},{"instruction":"MOV","oprands":"@Ri,A","description":"Move Accumulator to indirect RAM","oscillatorPeriod":12,"sizeInByte":1,"ID":10},{"instruction":"MOV","oprands":"@Ri,direct","description":"Move direct byte to indirect RAM","oscillatorPeriod":24,"sizeInByte":2,"ID":11},{"instruction":"MOV","oprands":"@Ri,#data","description":"Move immediate data to indirect RAM","oscillatorPeriod":12,"sizeInByte":2,"ID":12},{"instruction":"MOV","oprands":"DPTR,#data16","description":"Load Data Pointer with a 16-bit constant","oscillatorPeriod":24,"sizeInByte":3,"ID":13},{"instruction":"MOVC","oprands":"A,@A+DPTR","description":"Move Code byte relative to DPTR to Acc","oscillatorPeriod":24,"sizeInByte":1,"ID":14},{"instruction":"MOVC","oprands":"A,@A+PC","description":"Move Code byte relative to PC to Acc","oscillatorPeriod":24,"sizeInByte":1,"ID":15},{"instruction":"MOVX","oprands":"A,@Ri","description":"Move External RAM (8-bit add) to Acc","oscillatorPeriod":24,"sizeInByte":1,"ID":16},{"instruction":"MOVX","oprands":"A,@DPTR","description":"Move External RAM (16-bit add) to Acc","oscillatorPeriod":24,"sizeInByte":1,"ID":17},{"instruction":"MOVX","oprands":"@Ri,A","description":"Move Acc to External RAM (8-bit addr)","oscillatorPeriod":24,"sizeInByte":1,"ID":18},{"instruction":"MOVX","oprands":"@DPTR,A","description":"Move Acc to External RAM (16-bit addr)","oscillatorPeriod":24,"sizeInByte":1,"ID":19},{"instruction":"PUSH","oprands":"direct","description":"Push direct byte onto stack","oscillatorPeriod":24,"sizeInByte":2,"ID":20},{"instruction":"POP","oprands":"direct","description":"Pop direct byte onto stack","oscillatorPeriod":24,"sizeInByte":2,"ID":21},{"instruction":"XCH","oprands":"A,Rn","description":"Exchange register with Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":22},{"instruction":"XCH","oprands":"A,direct","description":"Exchange direct byte with Accumulator","oscillatorPeriod":12,"sizeInByte":2,"ID":23},{"instruction":"XCH","oprands":"Ai,@Ri","description":"Exchange indirect RAM with Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":24},{"instruction":"XCHD","oprands":"A,@Ri","description":"Exchange low-order Digit indirect RAM with Accumulator","oscillatorPeriod":12,"sizeInByte":1,"ID":25}]');
 
@@ -45,5 +45,55 @@ programbranchOps.forEach(function( obj ) {
 function craftTableRow(inst, oprand, des, byte, oscPeriod) {
     var trElement = $('<tr>');
     trElement.append($('<td>').text(inst));
+    trElement.append($('<td>').text(oprand));
+    trElement.append($('<td>').text(des));
+    trElement.append($('<td>').text(byte));
+    trElement.append($('<td>').text(oscPeriod));
+    
+    return trElement;
 }
 
+// Add Arithmetic instruction into table
+var arith_inst = $('#arith_inst');
+arithmeticOps.forEach( function( inst ) {
+    arith_inst.append( craftTableRow( 
+        inst.instruction,
+        inst.oprands,
+        inst.description,
+        inst.sizeInByte,
+        inst.oscillatorPeriod
+    ));
+});
+
+var data_inst = $('#data_inst');
+datatransferOps.forEach( function( inst ) {
+    data_inst.append( craftTableRow(
+        inst.instruction,
+        inst.oprands,
+        inst.description,
+        inst.sizeInByte,
+        inst.oscillatorPeriod     
+    ));
+});
+
+var logical_inst = $('#logical_inst');
+logicalOps.forEach( function( inst ) {
+    logical_inst.append( craftTableRow(
+        inst.instruction,
+        inst.oprands,
+        inst.description,
+        inst.sizeInByte,
+        inst.oscillatorPeriod
+    ));
+});
+
+var probranch_inst = $('#probranch_inst');
+programbranchOps.forEach( function( inst ) {
+    probranch_inst.append( craftTableRow(
+        inst.instruction,
+        inst.oprands,
+        inst.description,
+        inst.sizeInByte,
+        inst.oscillatorPeriod
+    ));
+});
